2417379a84b8fbba9392e6b3ae8753b413c1e2f9
poka <khs0826@dsm.hs.kr>
2023-12-14 11:58:48 +0900

    edit test

--- main_test.go
+++ main_test.go
@@ -6,41 +6,29 @@ import (
 )
 
 func BenchmarkStartWithMonitor(b *testing.B) {
-	start := time.Now()
-
 	scanner := MonitorScanner{}
 	for n := 0; n < b.N; n++ {
 		scanner.StartScan("127.0.0.1", 1, 65535, 500*time.Millisecond)
 	}
-	b.ReportMetric(float64(time.Since(start).Milliseconds()), "ms")
 }
 
 func BenchmarkStartWithSemaphore(b *testing.B) {
-	start := time.Now()
-
 	scanner := SemaphoreScanner{}
 	for n := 0; n < b.N; n++ {
 		scanner.StartScan("127.0.0.1", 1, 65535, 500*time.Millisecond)
 	}
-	b.ReportMetric(float64(time.Since(start).Milliseconds()), "ms")
 }
 
 func BenchmarkStartWithMutex(b *testing.B) {
-	start := time.Now()
-
 	scanner := MutexScanner{}
 	for n := 0; n < b.N; n++ {
 		scanner.StartScan("127.0.0.1", 1, 65535, 500*time.Millisecond)
 	}
-	b.ReportMetric(float64(time.Since(start).Milliseconds()), "ms")
 }
 
 func BenchmarkStartWithSequential(b *testing.B) {
-	start := time.Now()
-
 	scanner := SequentialScanner{}
 	for n := 0; n < b.N; n++ {
 		scanner.StartScan("127.0.0.1", 1, 65535, 500*time.Millisecond)
 	}
-	b.ReportMetric(float64(time.Since(start).Milliseconds()), "ms")
 }
